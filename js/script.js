96304503293bf4cbec98a20b4185dd8f689619ce
Seafnox <kirill9990@gmail.com>
2014-08-07 14:50:31 +0400

    update script

--- js/script.js
+++ js/script.js
@@ -239,8 +239,11 @@ $(document).ready(function()
         $('a[href*="novichok"]').on('click', function() {
             ga('send', 'event', 'заказ', 'клик на кнопке заказа', 'business | тариф Новичок');
         });
-        $('a[href*="VIP"]').on('click', function() {
+        $('a[href*="vip"]').on('click', function() {
             ga('send', 'event', 'заказ', 'клик на кнопке заказа', 'business | тариф VIP');
+        });
+        $('a[href*="ultra"]').on('click', function() {
+            ga('send', 'event', 'заказ', 'клик на кнопке заказа', 'business | тариф Ultra-VIP');
         });
         // --
         $("iframe").each(function(){var ifr_source=$(this).attr('src');var wmode="wmode=transparent";if(ifr_source.indexOf('?')!=-1){var getQString=ifr_source.split('?');var oldString=getQString[1];var newString=getQString[0];$(this).attr('src',newString+'?'+wmode+'&'+oldString)}else $(this).attr('src',ifr_source+'?'+wmode)});
